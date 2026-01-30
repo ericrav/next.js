dd8df4afd9ba2905977d388851df19690203259a
Will Binns-Smith <wbinnssmith@gmail.com>
2026-01-29 20:11:58 -0800

    Gate server HMR behind experimentalServerFastRefresh flag

--- packages/next/src/server/dev/hot-reloader-turbopack.ts
+++ packages/next/src/server/dev/hot-reloader-turbopack.ts
@@ -289,7 +289,8 @@ export async function createHotReloaderTurbopack(
   serverFields: ServerFields,
   distDir: string,
   resetFetch: () => void,
-  lockfile: Lockfile | undefined
+  lockfile: Lockfile | undefined,
+  experimentalServerFastRefresh?: boolean
 ): Promise<NextJsHotReloaderInterface> {
   const dev = true
   const buildId = 'development'
@@ -539,7 +540,9 @@ export async function createHotReloaderTurbopack(
     }
   }
 
-  setupServerHmr(project)
+  if (experimentalServerFastRefresh) {
+    setupServerHmr(project)
+  }
 
   let hmrEventHappened = false
   let hmrHash = 0
--- packages/next/src/server/lib/router-utils/setup-dev-bundler.ts
+++ packages/next/src/server/lib/router-utils/setup-dev-bundler.ts
@@ -111,6 +111,7 @@ export type SetupOpts = {
   port: number
   onDevServerCleanup: ((listener: () => Promise<void>) => void) | undefined
   resetFetch: () => void
+  experimentalServerFastRefresh?: boolean
 }
 
 export interface DevRoutesManifest {
@@ -216,7 +217,8 @@ async function startWatcher(
           serverFields,
           distDir,
           resetFetch,
-          lockfile
+          lockfile,
+          opts.experimentalServerFastRefresh
         )
       })()
     : await (async () => {
