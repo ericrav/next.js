d29511a7cffc31823fbe0f65ead1f835d6443951
Janka Uryga <lolzatu2@gmail.com>
2026-01-30 14:02:09 +0100

    more tests for blocking inside {static,runtime}

--- test/e2e/app-dir/instant-validation/instant-validation.test.ts
+++ test/e2e/app-dir/instant-validation/instant-validation.test.ts
@@ -382,6 +382,82 @@ describe('instant validation', () => {
        }
       `)
     })
+
+    it('valid - blocking page inside a static layout is allowed if the layout has suspense', async () => {
+      const browser = await next.browser('/static/valid-blocking-inside-static')
+      await browser.elementByCss('main')
+      await waitForNoErrorToast(browser)
+    })
+    it('valid - blocking page inside a runtime layout is allowed if the layout has suspense', async () => {
+      const browser = await next.browser(
+        '/runtime/valid-blocking-inside-runtime'
+      )
+      await browser.elementByCss('main')
+      await waitForNoErrorToast(browser)
+    })
+
+    it('invalid - blocking page inside a static layout is not allowed if the layout has no suspense', async () => {
+      const browser = await next.browser(
+        '/static/invalid-blocking-inside-static'
+      )
+      await expect(browser).toDisplayCollapsedRedbox(`
+       {
+         "description": "Runtime data was accessed outside of <Suspense>
+
+       This delays the entire page from rendering, resulting in a slow user experience. Next.js uses this error to ensure your app loads instantly on every navigation. cookies(), headers(), and searchParams, are examples of Runtime data that can only come from a user request.
+
+       To fix this:
+
+       Provide a fallback UI using <Suspense> around this component.
+
+       or
+
+       Move the Runtime data access into a deeper component wrapped in <Suspense>.
+
+       In either case this allows Next.js to stream its contents to the user when they request the page, while still providing an initial UI that is prerendered and prefetchable for instant navigations.
+
+       Learn more: https://nextjs.org/docs/messages/blocking-route",
+         "environmentLabel": "Server",
+         "label": "Blocking Route",
+         "source": "app/(plain-root)/static/invalid-blocking-inside-static/page.tsx (6:16) @ BlockingPage
+       > 6 |   await cookies()
+           |                ^",
+         "stack": [
+           "BlockingPage app/(plain-root)/static/invalid-blocking-inside-static/page.tsx (6:16)",
+         ],
+       }
+      `)
+    })
+    it('invalid - blocking page inside a runtime layout is not allowed if the layout has no suspense', async () => {
+      const browser = await next.browser(
+        '/runtime/invalid-blocking-inside-runtime'
+      )
+      await expect(browser).toDisplayCollapsedRedbox(`
+       {
+         "description": "Data that blocks navigation was accessed outside of <Suspense>
+
+       This delays the entire page from rendering, resulting in a slow user experience. Next.js uses this error to ensure your app loads instantly on every navigation. Uncached data such as fetch(...), cached data with a low expire time, or connection() are all examples of data that only resolve on navigation.
+
+       To fix this, you can either:
+
+       Provide a fallback UI using <Suspense> around this component. This allows Next.js to stream its contents to the user as soon as it's ready, without blocking the rest of the app.
+
+       or
+
+       Move the asynchronous await into a Cache Component ("use cache"). This allows Next.js to statically prerender the component as part of the HTML document, so it's instantly visible to the user.
+
+       Learn more: https://nextjs.org/docs/messages/blocking-route",
+         "environmentLabel": "Server",
+         "label": "Blocking Route",
+         "source": "app/(suspense-in-root)/runtime/invalid-blocking-inside-runtime/page.tsx (6:19) @ BlockingPage
+       > 6 |   await connection()
+           |                   ^",
+         "stack": [
+           "BlockingPage app/(suspense-in-root)/runtime/invalid-blocking-inside-runtime/page.tsx (6:19)",
+         ],
+       }
+      `)
+    })
   })
 
   describe('invalid - missing suspense in parallel slot', () => {
